8c259149ca055b84b5f6e84e8204d8eee594f278
ScorchingShade <ankushors789@gmail.com>
2019-05-10 01:43:10 +0530

    first module, printing something

---
+++ Prac1_Basics_print.go
@@ -0,0 +1,10 @@
+package main
+
+//we need main package and fmt or format import to print anything basically.
+//GO isn't object oriented , does not have classes and shit, so like make functions.
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, go")
+}
