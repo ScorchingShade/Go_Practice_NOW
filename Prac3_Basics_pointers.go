8c763be05e5188060856f7288ec49a3ee6c7fe41
ScorchingShade <ankushors789@gmail.com>
2019-05-11 05:07:27 +0530

    Pointers done

--- Prac3_Basics_pointers.go
+++ Prac3_Basics_pointers.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"fmt"
+)
+
+func pointer_test() {
+	// to specify a pointer simply use & equivalent to a var
+	x := 5
+	a := &x
+	//print var
+	fmt.Println(x)
+	// print value to pointer
+	fmt.Println(*a)
+	// print address
+	fmt.Println(&a)
+}
+
+func main() {
+
+	pointer_test()
+}
