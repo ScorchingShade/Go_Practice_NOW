ae656c329644a0d499b99dd86be524c9a24f1e32
ScorchingShade <ankushors789@gmail.com>
2019-05-11 05:59:12 +0530

    Will someday do a tough linkedlist as I learn go

---
+++ Prac4_Tough_DataStructures_linkedlist.go
@@ -0,0 +1,14 @@
+package main
+
+// coding a linked list like we do  in c. Trying to remember stuff
+
+type Node struct {
+	data int
+	next *Node
+}
+
+func beginning(data int) {
+
+	temp := new(Node)
+
+}
