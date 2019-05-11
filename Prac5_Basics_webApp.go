5f8b2602146eff0c447f037c99a7502081796c07
ScorchingShade <ankushors789@gmail.com>
2019-05-12 04:13:15 +0530

    Web basics completed

--- Prac5_Basics_webApp.go
+++ Prac5_Basics_webApp.go
@@ -8,11 +8,18 @@ import (
 	"net/http"
 )
 
+//The index_handler function contains the data of web page to load.
 // http.ResponseWriter is a custom data type, *http.Request is not pointer but actually reading through the request
-func index_handler(w http.ResponseWriter, r *http.Request)
+func index_handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1> This is a webpage</h1>")
+}
 
+//This fuction initialise our webapp
+//The HandleFunc of http requires net/http library
+//Handlefunc gives the starting index of webpage, i.e the address.
+//the ListenAndServe opens the port to present our webpage.
 func webApp() {
-	http.HandleFunc("/", index_handler)
+	http.HandleFunc("/admin", index_handler)
 	http.ListenAndServe(":8000", nil)
 }
 
