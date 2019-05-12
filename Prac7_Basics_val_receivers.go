46e6dbfed80f9bc6154a1eacf738672b9923bc7a
ScorchingShade <ankushors789@gmail.com>
2019-05-12 17:44:13 +0530

    done method receivers

--- Prac7_Basics_val_receivers.go
+++ Prac7_Basics_val_receivers.go
@@ -1,1 +1,28 @@
 package main
+
+import "fmt"
+
+const time int = 60
+
+type car struct {
+	Name  string
+	gas   int
+	price int
+	speed int
+}
+
+//this is a value receiver, it only does calculations in a struct. To do modifications in struct use pointer receiver
+func (c car) kmh() int {
+	return c.gas * (c.speed / time)
+}
+
+func main() {
+	p1 := car{"Ferrari", 50, 10, 300}
+
+	fmt.Println(p1)
+	fmt.Println(p1.Name)
+	fmt.Println(p1.gas)
+	fmt.Println(p1.price)
+
+	fmt.Println(p1.kmh())
+}
