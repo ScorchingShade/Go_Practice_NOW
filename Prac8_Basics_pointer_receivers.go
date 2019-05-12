738b2d93365ecf8887c229a8ed64b2e4e0bfde7e
ScorchingShade <ankushors789@gmail.com>
2019-05-12 19:13:36 +0530

    done pointer receiver

--- Prac8_Basics_pointer_receivers.go
+++ Prac8_Basics_pointer_receivers.go
@@ -11,11 +11,14 @@ type car1 struct {
 	speed int
 }
 
-//this is a value receiver, it only does calculations in a struct. To do modifications in struct use pointer receiver
+// This is a pointer receiver an modifies value in struct
 func (c car1) kmh1() int {
 	return c.gas * (c.speed / time1)
 }
 
+func (c *car1) new_speed(speed int) {
+	c.speed = speed
+}
 func main() {
 	p1 := car1{"Ferrari", 50, 10, 300}
 
@@ -25,4 +28,7 @@ func main() {
 	fmt.Println(p1.price)
 
 	fmt.Println(p1.kmh1())
+	p1.new_speed(200)
+	fmt.Println(p1.kmh1())
+
 }
