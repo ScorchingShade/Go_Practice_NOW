abad5fcaec03d18f66b8ac8a910ed588cadfb450
ScorchingShade <ankushors789@gmail.com>
2019-05-19 05:14:14 +0530

    learning html templates

--- Prac13_Basics_continue_web_app.go
+++ Prac13_Basics_continue_web_app.go
@@ -12,19 +12,19 @@ import (
 //we cleaned up the previous code to make it more lean
 //Goal- visiting sitemap links
 
+//since we had only one string to pull,we do it here only and reference loc tag og xml
+// we also delete the overriding function  as that fetched us strings. Dude we already got strings here.
+
 type SitemapIndexlean struct {
-	//since we had only one string to pull,we do it here only and reference loc tag og xml
-	// we also delete the overriding function  as that fetched us strings. Dude we already got strings here.
 	Locations []string `xml:"sitemap>loc"`
 }
 
 type News struct {
-	Titles    []string `xml:"url>news>title"`
-	Keywords  []string `xml:"url>news>Keywords"`
-	Locations []string `xml:"url>loc"`
+	Titles    []string `xml:"urlset>url>news>news>title"`
+	Keywords  []string `xml:"urlset>url>news>news>keywords"`
+	Locations []string `xml:"urlset>url>loc"`
 }
 
-//custom struct for mapping newsdata
 type NewsMap struct {
 	Keyword  string
 	Location string
@@ -33,7 +33,6 @@ type NewsMap struct {
 func main() {
 	var s SitemapIndexlean
 	var n News
-	//creating a map
 	news_map := make(map[string]NewsMap)
 
 	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemaps/index.xml")
@@ -41,21 +40,18 @@ func main() {
 	xml.Unmarshal(bytes, &s)
 
 	for _, Location := range s.Locations {
-		resp, err := http.Get(Location)
-		fmt.Println(err)
+		resp, _ := http.Get(Location)
 		bytes, _ := ioutil.ReadAll(resp.Body)
 		xml.Unmarshal(bytes, &n)
-		for idx, _ := range n.Titles {
+
+		for idx, _ := range n.Keywords {
 			news_map[n.Titles[idx]] = NewsMap{n.Keywords[idx], n.Locations[idx]}
 		}
-
 	}
 
 	for idx, data := range news_map {
 		fmt.Println("\n\n\n", idx)
 		fmt.Println("\n", data.Keyword)
 		fmt.Println("\n", data.Location)
-
 	}
-
 }
