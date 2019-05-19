aa822cc7fda01363082238cfcab2d8078b502e1f
ScorchingShade <ankushors789@gmail.com>
2019-05-20 03:52:30 +0530

    completed Sync. Will document later

--- Prac18_Basics_Synchronisation.go
+++ Prac18_Basics_Synchronisation.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+var wg sync.WaitGroup
+
+func saySync(s string) {
+	for i := 0; i < 3; i++ {
+		fmt.Println(s)
+		time.Sleep(time.Millisecond * 100)
+	}
+	wg.Done()
+}
+
+func main() {
+	wg.Add(1)
+	go saySync("hey")
+	wg.Add(1)
+	go saySync("there")
+	wg.Wait()
+
+	wg.Add(1)
+	go saySync("There")
+	wg.Wait()
+}
