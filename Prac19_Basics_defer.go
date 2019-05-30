6f387c756dd05b96ae6a56efd1fe7c44372ec890
ScorchingShade <ankushors789@gmail.com>
2019-05-31 00:09:21 +0530

    added some doc for prac19

--- Prac19_Basics_defer.go
+++ Prac19_Basics_defer.go
@@ -1,1 +1,24 @@
 package main
+
+// the defer statement will be evaluated when it gets hit, but wont complete till the surrounding function either completes or panics
+
+import (
+	"fmt"
+)
+
+func foo() {
+
+	//the defer statement is clearly 1st in priority since its written first, but it actually will only be evaluated after the entire function runs.
+	//this means, first the "Doing Something" statement will be evaluated.
+	//if there are multiple defers, it follows first in last out order
+
+	//that means the last defer statement in order will be executed the first
+
+	defer fmt.Println("Seriously defer")
+	defer fmt.Println("Defering something")
+	fmt.Println("Doing something")
+}
+
+func main() {
+	foo()
+}
