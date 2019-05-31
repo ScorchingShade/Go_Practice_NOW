0db504e4facc08d91070ce58abf85da97edb6e80
ScorchingShade <ankushors789@gmail.com>
2019-05-31 13:03:43 +0530

    added buffering code

--- Prac21_Basics_channels.go
+++ Prac21_Basics_channels.go
@@ -1,1 +1,23 @@
 package main
+
+import "fmt"
+
+func foo1(c chan int, someValue int) {
+	c <- someValue * 5
+
+}
+
+func main() {
+	fooVal := make(chan int)
+
+	go foo1(fooVal, 5)
+
+	go foo1(fooVal, 3)
+
+	//v1 := <- fooVal
+	//v2 := <- fooVal
+
+	v1, v2 := <-fooVal, <-fooVal
+
+	fmt.Println(v1, v2)
+}
