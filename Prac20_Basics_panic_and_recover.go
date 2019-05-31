1e9a43aab59427f60c47b54d061338002cb8b69d
ScorchingShade <ankushors789@gmail.com>
2019-05-31 12:52:52 +0530

    added code for panic and recover

--- Prac20_Basics_panic_and_recover.go
+++ Prac20_Basics_panic_and_recover.go
@@ -1,3 +1,45 @@
 package main
 
 //coming soon
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+var wg2 sync.WaitGroup
+
+func cleanup() {
+	if r := recover(); r != nil {
+		fmt.Println("Recovered in cleanup", r)
+	}
+
+	wg2.Done()
+}
+
+func say2(s string) {
+	defer cleanup()
+
+	for i := 0; i < 3; i++ {
+		time.Sleep(100 * time.Millisecond)
+		fmt.Println(s)
+
+		if i == 2 {
+			panic("oh dear, a 2")
+		}
+	}
+}
+
+func main() {
+
+	wg2.Add(1)
+	go say2("This is first")
+	wg2.Add(1)
+	go say2("This is second")
+	wg2.Wait()
+
+	wg2.Add(1)
+	go say2("This is third")
+	wg2.Wait()
+}
