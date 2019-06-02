5ad2feb421ac5feac231cb9691e363740b5f1311
ScorchingShade <ankushors789@gmail.com>
2019-06-02 23:00:00 +0530

    added some code, may or may not work

--- Prac23_Basics_Adding_Concurrency_to_web_app.go
+++ Prac23_Basics_Adding_Concurrency_to_web_app.go
@@ -1,1 +1,144 @@
 package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"net/http"
+	"sync"
+)
+
+var wg4 sync.WaitGroup
+var washpostXMLCom1 = []byte(`
+<sitemapindex>
+   <sitemap>
+      <loc>http://www.washingtonpost.com/news-politics-sitemap.xml</loc>
+   </sitemap>
+   <sitemap>
+      <loc>http://www.washingtonpost.com/news-blogs-technology-sitemap.xml</loc>
+   </sitemap>
+   <sitemap>
+      <loc>http://www.washingtonpost.com/news-opinions-sitemap.xml</loc>
+   </sitemap>
+</sitemapindex>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+xmlns:n="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd
+http://www.google.com/schemas/sitemap-news/0.9
+http://www.google.com/schemas/sitemap-news/0.9/sitemap-news.xsd">
+<url>
+<loc>https://www.washingtonpost.com/business/technology/un-adds-32-items-to-list-of-prohibited-goods-for-north-korea/2017/10/23/5f112818-b812-11e7-9b93-b97043e57a22_story.html</loc>
+<changefreq>hourly</changefreq>
+<n:news>
+<n:publication>
+<n:name>Washington Post</n:name>
+<n:language>en</n:language>
+</n:publication>
+<n:publication_date>2017-10-23T22:12:20Z</n:publication_date>
+<n:title>UN adds 32 items to list of prohibited goods for North Korea</n:title>
+<n:keywords>
+UN-United Nations-North Korea-Sanctions,North Korea,East Asia,Asia,United Nations Security Council,United Nations,Business,General news,Sanctions and embargoes,Foreign policy,International relations,Government and politics,Government policy,Military technology,Technology</n:keywords>
+</n:news>
+</url>
+<url>
+<loc>https://www.washingtonpost.com/business/technology/cisco-systems-buying-broadsoft-for-19-billion-cash/2017/10/23/ae024774-b7f2-11e7-9b93-b97043e57a22_story.html</loc>
+<changefreq>hourly</changefreq>
+<n:news>
+<n:publication>
+<n:name>Washington Post</n:name>
+<n:language>en</n:language>
+</n:publication>
+<n:publication_date>2017-10-23T21:42:14Z</n:publication_date>
+<n:title>Cisco Systems buying BroadSoft for $1.9 billion cash</n:title>
+<n:keywords>
+US-Cisco-BroadSoft-Acquisition,Cisco Systems Inc,Business,Technology,Communication technology</n:keywords>
+</n:news>
+</url>
+</urlset>`)
+
+type SitemapIndexCom1 struct {
+	//we make a location type array to store data from sitemap>location
+	Locations []string `xml:"sitemap>loc"`
+}
+
+type NewsCom1 struct {
+	//We create a second datatype news to store titles, keywords and locations
+	Titles    []string `xml:"url>news>title"`
+	Keywords  []string `xml:"url>news>keywords"`
+	Locations []string `xml:"url>loc"`
+}
+
+type NewsMapCom1 struct {
+	//This is to iterate through our datatype values
+	Keyword  string
+	Location string
+}
+
+//generating a struct to pass data to the html files
+type NewsAggPageCom1 struct {
+	Title string
+	News  map[string]NewsMapCom1
+}
+
+//a basic index page , this is not a template
+func index_handlerCom1(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1 align=\"center\">Templating</h1>")
+}
+
+func newsRoutine(c chan NewsCom1, Location string) {
+	defer wg4.Done()
+	var n NewsCom1
+	resp, _ := http.Get(Location)
+	bytes := washpostXMLCom1
+	xml.Unmarshal(bytes, &n)
+	resp.Body.Close()
+	c <- n
+}
+
+func newsAggHandlerCom1(w http.ResponseWriter, r *http.Request) {
+
+	var s SitemapIndexCom1
+	var n NewsCom1
+	news_map := make(map[string]NewsMapCom1)
+	bytes := washpostXMLCom1
+	xml.Unmarshal(bytes, &s)
+	for _, Location := range s.Locations {
+		resp, _ := http.Get(Location)
+		bytes, _ := ioutil.ReadAll(resp.Body)
+		xml.Unmarshal(bytes, &n)
+		queue := make(chan NewsCom1, 30)
+
+		for _, Location := range s.Locations {
+			wg4.Add(1)
+			go newsRoutine(queue, Location)
+		}
+		wg4.Wait()
+		close(queue)
+
+		for elem := range queue {
+			for idx, _ := range elem.Keywords {
+				news_map[elem.Titles[idx]] = NewsMapCom1{elem.Keywords[idx], elem.Locations[idx]}
+			}
+		}
+
+		for idx, _ := range n.Titles {
+			news_map[n.Titles[idx]] = NewsMapCom1{n.Keywords[idx], n.Locations[idx]}
+
+		}
+	}
+
+	p := NewsAggPageCom1{Title: "Washington Post News Aggregator", News: news_map}
+	t, _ := template.ParseFiles("/root/Desktop/Projects/Go_Pros/Practice/Prac16_Basics_using_templates_news_temp.html")
+
+	fmt.Println(t.Execute(w, p))
+}
+
+func main() {
+
+	http.HandleFunc("/", index_handlerCom1)
+	http.HandleFunc("/agg", newsAggHandlerCom1)
+	http.ListenAndServe(":8000", nil)
+
+}
