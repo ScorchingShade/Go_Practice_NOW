11872546829c7a0e4e58d339c3aeecfd56dfbc70
ScorchingShade <ankushors789@gmail.com>
2019-06-02 20:47:26 +0530

    adding code for buffers

--- Prac22_Basics_Buffering_and_iterating_over_channels.go
+++ Prac22_Basics_Buffering_and_iterating_over_channels.go
@@ -1,3 +1,23 @@
 package main
 
 //buffering start
+
+import "fmt"
+
+func foo3(c chan int, someValue int) {
+	c <- someValue * 5
+
+}
+
+func main() {
+
+	fooVal := make(chan int)
+
+	for i := 0; i < 10; i++ {
+		go foo3(fooVal, i)
+	}
+
+	for item := range fooVal {
+		fmt.Println(item)
+	}
+}
