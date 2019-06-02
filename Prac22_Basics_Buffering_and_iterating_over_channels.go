2c143ce1e9dce5f5a01228671b7eb3b623dcda83
ScorchingShade <ankushors789@gmail.com>
2019-06-02 20:53:20 +0530

    completed buffers

--- Prac22_Basics_Buffering_and_iterating_over_channels.go
+++ Prac22_Basics_Buffering_and_iterating_over_channels.go
@@ -2,22 +2,33 @@ package main
 
 //buffering start
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
+
+var wg3 sync.WaitGroup
 
 func foo3(c chan int, someValue int) {
+	defer wg3.Done()
 	c <- someValue * 5
 
 }
 
 func main() {
 
-	fooVal := make(chan int)
+	fooVal := make(chan int, 10)
 
 	for i := 0; i < 10; i++ {
+		wg3.Add(1)
 		go foo3(fooVal, i)
 	}
-
+	wg3.Wait()
+	close(fooVal)
 	for item := range fooVal {
 		fmt.Println(item)
 	}
+
+	//time.Sleep(time.Second*2)
+
 }
